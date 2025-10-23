1d0a1eb4f27ac9394cc2f86e4ee5954d135447f0
ThomasDuvinage <toto.duvinage@gmail.com>
2025-10-23 14:07:15 +0900

    update: clear-omninames.sh

--- projects/JVRC1/cnoid/clear-omninames.sh
+++ projects/JVRC1/cnoid/clear-omninames.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-sudo systemctl stop omniorb-nameserver
-sudo rm -f /var/log/omniorb-nameserver.log 2> /dev/null
-sudo rm -f /var/lib/omniorb/* 2> /dev/null
-sudo rm -f /tmp/rtcmanager.ref 2> /dev/null
-sudo systemctl start omniorb-nameserver
-for i in {0..9}
-do
-    [ -f /var/log/omniorb-nameserver.log ] && break
-    sleep 1
-done
-tail /var/log/omniorb-nameserver.log
+echo "Killing all existing omniNames processes"
+killall omniNames
+echo "Starting omniNames with a clean slate:"
+mkdir -p /tmp/omninames.dat
+echo "- Removing omninames data file: /tmp/omninames.dat"
+rm -f /tmp/omninames.dat/*
+echo "- Removing omninames log file: /tmp/omninames.log"
+rm -f /tmp/omninames.log
+
+echo "- Starting omniNames in the background"
+# Start omninames in the background
+omniNames \
+    -start 2809 \
+    -always \
+    -errlog /tmp/omninames.log \
+    -datadir /tmp/omninames.dat \
+    -nohostname &
+pid="$!"
+echo "omniNames pid is $pid"
+echo "- Waiting 1s for omniNames to start."
+sleep 1
+echo "- Please check the following log file for errors:"
+tail /tmp/omninames.log
